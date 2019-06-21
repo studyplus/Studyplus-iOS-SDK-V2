593801dd07b6af56c3c9483361f9f4a93295f053
Koichiro Oishi <koichiro.oishi@studyplus.jp>
2019-06-21 16:31:40 +0900

    fix typo

--- StudyplusSDK/Studyplus.swift
+++ StudyplusSDK/Studyplus.swift
@@ -263,8 +263,8 @@ final public class Studyplus {
               let consumerKey = data["consumerKey"],
               let consumerSecret = data["consumerSecret"] else {
 
-            assert(false, "StudyplusSDK: *** Pleease set consumerKey and consumerSecret in your Info.plist. ***")
-            print("StudyplusSDK: *** Pleease set consumerKey and consumerSecret in your Info.plist. ***")
+            assert(false, "StudyplusSDK: *** Please set consumerKey and consumerSecret in your Info.plist. ***")
+            print("StudyplusSDK: *** Please set consumerKey and consumerSecret in your Info.plist. ***")
 
             self.consumerKey = ""
             self.consumerSecret = ""
@@ -273,8 +273,8 @@ final public class Studyplus {
         
         if consumerKey == "set_your_consumerKey" || consumerSecret == "set_your_consumerSecret" {
             
-            assert(false, "StudyplusSDK: *** Pleease set consumerKey and consumerSecret in Info.plist. ***")
-            print("StudyplusSDK: *** Pleease set consumerKey and consumerSecret in your Info.plist. ***")
+            assert(false, "StudyplusSDK: *** Please set consumerKey and consumerSecret in Info.plist. ***")
+            print("StudyplusSDK: *** Please set consumerKey and consumerSecret in your Info.plist. ***")
 
             self.consumerKey = ""
             self.consumerSecret = ""
